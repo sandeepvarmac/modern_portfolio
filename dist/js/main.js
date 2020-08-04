5e96443f6fa90ef08be45d0318fa45676c2f532d
Sandeep Varma <sandeepvarmac@gmail.com>
2020-08-04 09:52:01 +0530

    Menu Overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,11 @@
-// Select DOM items
+// Select DOM Items
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
 
-//set Initial state of Menu
+// Set Initial State Of Menu
 let showMenu = false;
 
 menuBtn.addEventListener("click", toggleMenu);
@@ -17,14 +17,16 @@ function toggleMenu() {
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
     navItems.forEach((item) => item.classList.add("show"));
+
     // Set Menu State
     showMenu = true;
   } else {
     menuBtn.classList.remove("close");
     menu.classList.remove("show");
     menuNav.classList.remove("show");
-    menuBranding.classList.remove("show)");
+    menuBranding.classList.remove("show");
     navItems.forEach((item) => item.classList.remove("show"));
+
     // Set Menu State
     showMenu = false;
   }
